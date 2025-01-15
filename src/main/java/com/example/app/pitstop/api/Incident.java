c95bee280e5ad8fb1932eadd8397228a56715fcc
Rene de Waele <rene@flux-capacitor.io>
2025-01-15 09:45:19 +0100

    Remove Flux annotations from classes in the pitstop api package

--- src/main/java/com/example/app/pitstop/api/Incident.java
+++ src/main/java/com/example/app/pitstop/api/Incident.java
@@ -1,9 +1,6 @@
 package com.example.app.pitstop.api;
 
 import com.example.app.user.api.UserId;
-import io.fluxcapacitor.javaclient.modeling.Aggregate;
-import io.fluxcapacitor.javaclient.modeling.EntityId;
-import io.fluxcapacitor.javaclient.modeling.Member;
 import lombok.Builder;
 import lombok.Singular;
 import lombok.Value;
@@ -12,11 +9,9 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
-@Aggregate(searchable = true, timestampPath = "start", endPath = "end")
 @Builder(toBuilder = true)
 @Value
 public class Incident {
-    @EntityId
     IncidentId incidentId;
     IncidentDetails details;
 
@@ -24,12 +19,12 @@ public class Incident {
 
     Instant start, end;
 
-    @Member
     @Singular
     List<Offer> offers;
 
     Assistance assistance;
 
+    @SuppressWarnings("unused")
     public Optional<Offer> getAcceptedOffer() {
         return offers.stream().filter(Offer::isAccepted).findFirst();
     }
