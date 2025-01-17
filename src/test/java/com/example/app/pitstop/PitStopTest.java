dc1f7eb6f82eca558eeef08bc3493ddb7ca474ef
Rene de Waele <rene@flux-capacitor.io>
2025-01-17 14:33:45 +0100

    Add test for CORS preflight request

--- src/test/java/com/example/app/pitstop/PitStopTest.java
+++ src/test/java/com/example/app/pitstop/PitStopTest.java
@@ -1,6 +1,8 @@
 package com.example.app.pitstop;
 
 import io.fluxcapacitor.javaclient.test.TestFixture;
+import io.fluxcapacitor.javaclient.web.HttpRequestMethod;
+import io.fluxcapacitor.javaclient.web.WebRequest;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -14,5 +16,10 @@ class PitStopTest {
         testFixture.whenGet("/api/incidents").<List<?>>expectResult(List::isEmpty);
     }
 
-
+    @Test
+    void testCorsPreflight() {
+        testFixture.whenWebRequest(WebRequest.builder().method(HttpRequestMethod.OPTIONS)
+                                           .url("/api/user").build())
+                .expectSuccessfulResult();
+    }
 }
